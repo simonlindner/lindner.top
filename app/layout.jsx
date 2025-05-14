5778d05f6dad309ab6de9775d27d5ec1b63c3b64
Simon Lindner <lindner@cyberdyne.de>
2025-05-15 01:32:03 +0200

    init Repo

---
+++ app/layout.jsx
@@ -0,0 +1,56 @@
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+config.autoAddCss = false;
+import "./nprogress.css";
+import { Analytics } from "@vercel/analytics/react";
+import ClientTopProgressBar from "@/components/ClientTopProgressBar";
+
+export const metadata = {
+    title: "Simon | Portofolio",
+
+    description:
+		"My name is Simon, I'm a it operations engineer. I'm currently working at the Cyberdyne IT GmbH.",
+
+    author: "Simon Lindner",
+    siteUrl: "https://www.lindner.top",
+    applicationName: "Simon",
+
+    keywords: [
+		"simon",
+		"simon lindner",
+		"simon lindner portofolio",
+		"lindner"
+	],
+
+    openGraph: {
+		type: "website",
+		url: "https:/lindner.top",
+		title: "Simon | Portofolio",
+		site_name: "Simon | Portofolio",
+		description: "My name is Simon, This is my portofolio website.",
+		width: 1200,
+		height: 630,
+		images: [
+			{
+				url: "/og-image-rev.png",
+				alt: "Simon Portofolio",
+			},
+		],
+		site_name: "Simon | Portofolio",
+	}
+};
+
+export default function RootLayout({ children }) {
+	return (
+		<html lang="en">
+			<body>
+				<ClientTopProgressBar />
+				<Navbar />
+				{children}
+				<Analytics />
+			</body>
+		</html>
+	);
+}
